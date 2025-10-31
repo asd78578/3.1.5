8b8551c33f98c433648c9a032f6d4e5837f792af
tricky <alavutdin055@gmail.com>
2025-11-01 01:01:25 +0300

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,62 +6,52 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestTemplate;
 
 public class Main {
-
-
     private static final String URL = "http://94.198.50.185:7081/api/users";
-
 
     public static void main(String[] args) {
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                URL,
-                HttpMethod.GET,
-                null,
-                String.class
-        );
+        try {
 
-        String sessionId = response.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+            ResponseEntity<String> getResponse = restTemplate.getForEntity(URL, String.class);
+            String cookies = getResponse.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set(HttpHeaders.COOKIE, sessionId);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.add(HttpHeaders.COOKIE, cookies);
 
+            StringBuilder resultCode = new StringBuilder();
 
-        User newUser = new User(3L, "James", "Brown", (byte) 25);
-        HttpEntity<User> addRequest = new HttpEntity<>(newUser, headers);
-        ResponseEntity<String> addResponse = restTemplate.exchange(
-                URL,
-                HttpMethod.POST,
-                addRequest,
-                String.class
-        );
-        System.out.println("Добавление пользователя " + addResponse.getBody());
+            User newUser = new User(3L, "James", "Brown", (byte) 25);
+            HttpEntity<User> postEntity = new HttpEntity<>(newUser, headers);
+            ResponseEntity<String> postResponse = restTemplate.postForEntity(URL, postEntity, String.class);
+            resultCode.append(postResponse.getBody());
+            System.out.println("Добавление пользователя " + postResponse.getBody());
 
+            User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 25);
+            HttpEntity<User> putEntity = new HttpEntity<>(updatedUser, headers);
+            ResponseEntity<String> putResponse = restTemplate.exchange(URL, HttpMethod.PUT, putEntity, String.class);
+            resultCode.append(putResponse.getBody());
+            System.out.println("Изменение пользователя " + putResponse.getBody());
 
-        User updateUser = new User(3L, "Thomas", "Shelby", (byte) 25);
-        HttpEntity<User> updateRequest = new HttpEntity<>(updateUser, headers);
-        ResponseEntity<String> updateResponse = restTemplate.exchange(
-                URL,
-                HttpMethod.PUT,
-                updateRequest,
-                String.class
-        );
-        System.out.println("Изменение пользователя " + updateResponse.getBody());
+            ResponseEntity<String> deleteResponse = restTemplate.exchange(
+                    URL + "/3",
+                    HttpMethod.DELETE,
+                    new HttpEntity<>(headers),
+                    String.class
+            );
+            resultCode.append(deleteResponse.getBody());
+            System.out.println("Удаление пользователя " + deleteResponse.getBody());
 
+            System.out.println("\nИтоговый ответ: " + resultCode.toString()
+            +"\u001B[31m" + "\nEND");
 
-        HttpEntity<String> deleteRequest = new HttpEntity<>(headers);
-        ResponseEntity<String> deleteResponse = restTemplate.exchange(
-                URL + "/3",
-                HttpMethod.DELETE,
-                deleteRequest,
-                String.class
-        );
-        System.out.println("Удаление пользователя " + deleteResponse.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("\nИТОГОВЫЙ ОТВЕТ: " + addResponse.getBody() + updateResponse.getBody() + deleteResponse.getBody());
 
     }
 }
